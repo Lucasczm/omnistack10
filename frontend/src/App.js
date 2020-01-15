1afd8478a782ce16fc22b9da3b6a00aa079c9240
Lucas Machado <lucascezarmachado@gmail.com>
2020-01-15 15:51:26 -0300

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import DevForm from "./components/DevForm";
+import DevItem from "./components/DevItem";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+    setDevs([...devs, response.data]);
+  }
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem dev={dev} key={dev.id} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
